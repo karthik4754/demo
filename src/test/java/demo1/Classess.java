2c8668d3f921949553c0aedf7099e753499cdfae
karthik4754 <karthikcreative14@gmail.com>
2020-05-05 09:35:27 +0100

    karthi2 updated

--- src/test/java/demo1/Classess.java
+++ src/test/java/demo1/Classess.java
@@ -3,6 +3,8 @@ package demo1;
 public class Classess {
 	public static void main(String[] args) {
 		System.out.println("java");
+		System.out.println("guru");
+		System.out.println("charan");
 	}
 
 
